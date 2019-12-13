387f2acb585cca0741bfdcdde0be05ee559531df
Charly LEE <charly.loggar@gmail.com>
2019-12-13 16:02:30 +1100

    feat (global)

---
+++ js-front-dev/global/global.1.js
@@ -0,0 +1,20 @@
+var getGlobal = function() {
+  if (typeof self !== "undefined") {
+    return self;
+  }
+  if (typeof window !== "undefined") {
+    return window;
+  }
+  if (typeof global !== "undefined") {
+    return global;
+  }
+  throw new Error("unable to locate global object");
+};
+
+var globals = getGlobal();
+
+if (typeof globals.setTimeout !== "function") {
+  console.log("no setTimeout in this environment! ");
+}
+
+console.log(globals);
---
+++ js-front-dev/global/global.2.globalThis.js
@@ -0,0 +1,5 @@
+if (typeof globalThis.setTimeout !== "function") {
+  // no setTimeout in this environment!
+}
+
+console.log(globalThis);
