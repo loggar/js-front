f66d0b60841f893db6e3dc43050cc5b46f0b36d4
Charly LEE <charly.loggar@gmail.com>
2020-01-31 13:08:08 +1100

    feat (clipboard)

--- js-front.refs/clipboard/copy-text-to-clipboard.js
+++ js-front.refs/clipboard/copy-text-to-clipboard.js
@@ -1,22 +1,9 @@
-const copyToClipboard = str => {
-  const el = document.createElement("textarea");
-  el.value = str;
-  el.setAttribute("readonly", "");
-  el.style.position = "absolute";
-  el.style.left = "-9999px";
-  document.body.appendChild(el);
-  const selected =
-    document.getSelection().rangeCount > 0
-      ? document.getSelection().getRangeAt(0)
-      : false;
-  el.select();
-  document.execCommand("copy");
-  document.body.removeChild(el);
-  if (selected) {
-    document.getSelection().removeAllRanges();
-    document.getSelection().addRange(selected);
-  }
+const listener = e => {
+  // e : ClipboardEvent
+  e.clipboardData.setData("text/plain", text);
+  e.preventDefault();
 };
 
-// Example
-copyToClipboard("Lorem ipsum"); // 'Lorem ipsum' copied to clipboard.
+document.addEventListener("copy", listener);
+document.execCommand("copy");
+document.removeEventListener("copy", listener);
