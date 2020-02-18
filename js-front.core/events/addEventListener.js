94505d00031c437eb5547a3cc901f7f71d935cfe
Charly LEE <charly.loggar@gmail.com>
2020-02-19 10:28:25 +1100

    refactor (event)

--- js-front.core/events/addEventListener.js
+++
@@ -1,8 +0,0 @@
-var b = document.getElementById("mybutton");
-b.onclick = function () { console.log("Thanks for clicking me!"); };
-b.addEventListener("click", function () { console.log("Thanks again!"); }, false);
-
-
-
-document.removeEventListener("mousemove", handleMouseMove, true);
-document.removeEventListener("mouseup", handleMouseUp, true);
