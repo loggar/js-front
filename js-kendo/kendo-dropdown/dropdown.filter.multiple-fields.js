bc7dbd63b86c9b4e9fe96b9ab9717652512a529f
Charly LEE <charly.loggar@gmail.com>
2020-10-23 11:42:48 +1100

    feat (kendo) dropdown, filter

--- js-kendo/kendo-dropdown/dropdown.filter.multiple-fields.js
+++ js-kendo/kendo-dropdown/dropdown.filter.multiple-fields.js
@@ -1,24 +1,25 @@
-{
-  dataTextField : "TRAD_NM",
-  dataValueField : "AGENT_ID",
-  template : kendo.template($("#agent_id_template").html()),
-  filter : "contains",
-  filtering : function(ev) {
+const config = {
+  dataTextField: "TRAD_NM",
+  dataValueField: "AGENT_ID",
+  template: kendo.template($("#agent_id_template").html()),
+  filter: "contains",
+  filtering: function (ev) {
     var filterValue = ev.filter != undefined ? ev.filter.value : "";
     ev.preventDefault();
     this.dataSource.filter({
-      logic : "or",
-      filters : [
+      logic: "or",
+      filters: [
         {
-          field : "AGENT_ID",
-          operator : "contains",
-          value : filterValue
-        }, {
-          field : "TRAD_NM",
-          operator : "contains",
-          value : filterValue
-        }
-      ]
+          field: "AGENT_ID",
+          operator: "contains",
+          value: filterValue,
+        },
+        {
+          field: "TRAD_NM",
+          operator: "contains",
+          value: filterValue,
+        },
+      ],
     });
-  }
-}
+  },
+};
