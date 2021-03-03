0d9180704719d57483d662a601d2468b378d44be
Charly LEE <charly.loggar@gmail.com>
2021-03-03 18:08:42 +1100

    feat (dom) node traverse

---
+++ js-front.dom/dom-traverse/dom.traverse.iterative.js
@@ -0,0 +1,10 @@
+function visit_nodes_iterative(callback, node) {
+  node = node || document;
+  do {
+      callback.call(null, node);
+      node = node.firstChild || node.nextSibling || function() {
+          while ((node = node.parentNode) && !node.nextSibling);
+          return node ? node.nextSibling : null;
+      }();
+  } while (node);
+}
---
+++ js-front.dom/dom-traverse/dom.traverse.recursive.js
@@ -0,0 +1,6 @@
+function visit_nodes_recursive(callback, node) {
+  node = node || document;
+  callback.call(null, node);
+  if (node.firstChild) visit_nodes_recursive(callback, node.firstChild);
+  if (node.nextSibling) visit_nodes_recursive(callback, node.nextSibling);
+}
