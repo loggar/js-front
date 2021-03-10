95639d8da9dbcd018b28ad50b52c1e8ca616b33e
Charly LEE <charly.loggar@gmail.com>
2021-03-10 13:50:44 +1100

    refactor (dirs)

--- style-css/supports/supports.1.js
+++
@@ -1,5 +0,0 @@
-if (CSS.supports("display: grid")) {
-  document.getElementsByClassName("main").style.display = "grid";
-} else {
-  document.getElementsByClassName("main").style.display = "flex";
-}
